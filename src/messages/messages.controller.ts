3b19093879a0feb81d55471ea92cae8c150f7873
bohdanvakh <bohdanvakh@gmail.com>
2026-08-15 19:01:20 +0300

    feat/add messages controller and module

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+    @Get()
+    index() {
+        
+    }
+
+    @Post()
+    create() {
+
+    }
+
+
+    @Get('/:id')
+    getMessage() {
+
+    }
+}
