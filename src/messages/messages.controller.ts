0e4f4af3361571db8fd2cb0b22a3165d00e3fabb
bohdanvakh <bohdanvakh@gmail.com>
2026-08-16 14:39:50 +0300

    feat/add validation

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
@@ -8,13 +9,13 @@ export class MessagesController {
     }
 
     @Post()
-    create() {
-
+    create(@Body() body: CreateMessageDto) {
+        console.log(body);
     }
 
 
     @Get('/:id')
-    getMessage() {
-
+    getMessage(@Param('id') id: string) {
+        console.log(id);
     }
 }
