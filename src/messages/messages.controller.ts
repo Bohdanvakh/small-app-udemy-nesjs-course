7e4ef005de353a49febf4cc9561fdc1f0d865c0b
bohdanvakh <bohdanvakh@gmail.com>
2026-08-18 23:31:50 +0300

    feat/add messages service to controller to create logic of creation and finding messages

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,21 +1,30 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dto/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+    messagesService: MessagesService;
+
+    constructor() {
+        // service is creating its own dependencies
+        // we don't do it in real apps, just in simple example of the cource
+        this.messagesService = new MessagesService();
+    }
+
     @Get()
     index() {
-        
+        return this.messagesService.findAll();
     }
 
     @Post()
     create(@Body() body: CreateMessageDto) {
-        console.log(body);
+        return this.messagesService.create(body.content);
     }
 
 
     @Get('/:id')
     getMessage(@Param('id') id: string) {
-        console.log(id);
+        return this.messagesService.findOne(id);
     }
 }
